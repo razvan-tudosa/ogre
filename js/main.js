fa3cad1e7150e6e2dc34010ec8876a1e27b737f2
razvan.tudosa <tudosa.razvan@gmail.com>
2014-01-27 21:11:22 +0200

    latest updates: integration with Bianca's work

--- js/main.js
+++ js/main.js
@@ -3,8 +3,6 @@
 
     var ogreAPI = {
         canvas : null,
-        obj: null,
-        charts: [],
         images: [],
         helper: {
             bgImage: null,
@@ -221,6 +219,126 @@
             }
         },
 
+        charts:{
+            chartElements: [],
+
+            options: {},
+
+            addFileButon: function (){
+             
+                ogreAPI.charts.options = {
+                    chart: {
+                        renderTo: 'container',
+                        defaultSeriesType: $('#chartType').val(),
+                        borderRadius: 0,
+                        backgroundColor: 'rgba(0,0,0,0)'
+                    },
+                    title: {
+                        text: ''
+                    },
+                    subtitle :{
+                        text: ''
+                    },
+                    xAxis: {
+                        categories: []
+                    },
+                    yAxis: {
+                        title: {
+                            text: ''
+                        }
+                    },
+                    series: [],
+                    credits: {
+                        enabled: false
+                    }
+                };
+
+                $('#file').on('change', function(evt) {
+    
+                    //Retrieve the first (and only!) File from the FileList object
+                    var f = evt.target.files[0];
+                    ogreAPI.file = f;
+                    ogreAPI.charts.options.series = [];
+                    ogreAPI.charts.options.title.text = $('#title').val();
+                    ogreAPI.charts.options.subtitle.text = $('#subtitle').val();
+
+                    if(f) {
+
+                        var r = new FileReader();
+
+                        r.onload = function(e) {
+                            var data = e.target.result;
+
+                            // Split the lines
+                            var lines = data.split('\n');
+
+                            // Iterate over the lines and add categories or series
+                            $.each(lines, function(lineNo, line) {
+                                var items = line.split(',');
+
+                                // header line containes categories
+                                if(lineNo == 0) {
+                                    $.each(items, function(itemNo, item) {
+                                        if (itemNo > 0) ogreAPI.charts.options.xAxis.categories.push(item);
+                                    });
+                                }
+
+                                    // the rest of the lines contain data with their name in the first 
+                                    // position
+                                else {
+
+                                    var series = {
+                                        data: []
+                                    };
+
+                                    $.each(items, function(itemNo, item) {
+                                        if (itemNo == 0) {
+                                            series.name = item;
+                                        } else {
+                                            series.data.push(parseFloat(item));
+                                        }
+                                    });
+
+                                    ogreAPI.charts.options.series.push(series);
+                                }
+                            });
+                            // Create the chart
+                            ogreAPI.chart = new Highcharts.Chart(ogreAPI.charts.options);
+                        };
+
+                        r.readAsText(f);
+                    }
+                });
+            },
+
+            dropdownOptions: function(){
+
+                $('#chartType').on('change', function() {
+
+                    ogreAPI.chart = $('#container').highcharts();
+
+                    if(ogreAPI.chart) {
+                        ogreAPI.chart.destroy();
+                        var chartType = $('#chartType').val();
+
+                        ogreAPI.charts.options.chart.defaultSeriesType = chartType;
+                        ogreAPI.charts.options.title.text = $('#title').val();
+                        ogreAPI.charts.options.subtitle.text = $('#subtitle').val();
+
+                        ogreAPI.chart = new Highcharts.Chart(ogreAPI.charts.options);
+                    }
+                });
+
+                $('#charts-panel #add-chart').on('click', function() {
+                    ogreAPI.loadInfographic();
+                });
+
+                $('#charts-panel .back-button').on('click', function() {
+                    $('#charts-wrapper').addClass('hide');
+                });
+            },
+        },
+
         exportInfographic: {
             _toSVG: function() {
                 var infographic = ogreAPI.canvas.toSVG();
@@ -252,28 +370,30 @@
             }
         },
             
-        loadDummyInfographic : function() {
+        loadInfographic : function() {
             var test = '<svg width="700" height="300" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" stroke="none" stroke-width="0" fill="none" class="dxc dxc-chart" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);"><defs><clipPath id="DevExpress_1"><rect x="0" y="0" width="700" height="300" rx="0" ry="0" fill="none" stroke="none" stroke-width="0"></rect></clipPath><clipPath id="DevExpress_2"><rect x="0" y="0" width="700" height="300" rx="0" ry="0" fill="none" stroke="none" stroke-width="0" transform="translate(-609,-12)"></rect></clipPath><pattern id="DevExpressPattern_1" width="5" height="5" patternUnits="userSpaceOnUse"><rect x="0" y="0" width="5" height="5" rx="0" ry="0" fill="#ffa500" opacity="0.75"></rect><path stroke-width="2" stroke="#ffa500" d="M 2.5 -2.5 L -2.5 2.5M 0 5 L 5 0 M 7.5 2.5 L 2.5 7.5"></path></pattern><pattern id="DevExpressPattern_2" width="5" height="5" patternUnits="userSpaceOnUse"><rect x="0" y="0" width="5" height="5" rx="0" ry="0" fill="#ffa500" opacity="0.5"></rect><path stroke-width="2" stroke="#ffa500" d="M 2.5 -2.5 L -2.5 2.5M 0 5 L 5 0 M 7.5 2.5 L 2.5 7.5"></path></pattern><clipPath id="DevExpress_3"><rect x="22" y="10" width="557" height="265" rx="0" ry="0" fill="none" stroke="none" stroke-width="0"></rect></clipPath><pattern id="DevExpressPattern_3" width="6" height="6" patternUnits="userSpaceOnUse"><rect x="0" y="0" width="6" height="6" rx="0" ry="0" fill="#ffa500" opacity="0.75"></rect><path stroke-width="2" stroke="#ffa500" d="M 3 -3 L -3 3M 0 6 L 6 0 M 9 3 L 3 9"></path></pattern><pattern id="DevExpressPattern_4" width="6" height="6" patternUnits="userSpaceOnUse"><rect x="0" y="0" width="6" height="6" rx="0" ry="0" fill="#ffa500" opacity="0.5"></rect><path stroke-width="2" stroke="#ffa500" d="M 3 -3 L -3 3M 0 6 L 6 0 M 9 3 L 3 9"></path></pattern></defs><g class="dxc-background"></g><g class="dxc-legend" transform="translate(609,12)" clip-path="url(#DevExpress_2)"><g><g class="dxc-item" transform="translate(0,0)"><rect x="0" y="0" width="12" height="12" rx="0" ry="0" fill="#ffa500"></rect><text x="19" y="0" text-anchor="start" style="fill: #808080; fill-opacity: 0.75; font-family: \'Segoe UI\', \'Helvetica Neue\', \'Trebuchet MS\', Verdana; font-weight: 400; font-size: 12px; cursor: default;" transform="translate(0,10)"><tspan x="19" dy="0">My oranges</tspan></text></g></g></g><g class="dxc-strips-group"><g class="dxc-h-strips" clip-path="url(#DevExpress_3)"></g><g class="dxc-v-strips" clip-path="url(#DevExpress_3)"></g></g><g class="dxc-constant-lines-group"><g class="dxc-h-constant-lines" clip-path="url(#DevExpress_3)"></g><g class="dxc-v-constant-lines" clip-path="url(#DevExpress_3)"></g></g><g class="dxc-axes-group"><g class="dxc-h-axis"><g class="dxc-grid"></g><g class="dxc-elements"><text x="62" y="297" text-anchor="middle" transform="rotate(0,62,297)" style="fill: #808080; fill-opacity: 0.75; font-family: \'Segoe UI\', \'Helvetica Neue\', \'Trebuchet MS\', Verdana; font-weight: 400; font-size: 12px; cursor: default;"><tspan x="62" dy="0">Monday</tspan></text><text x="141" y="297" text-anchor="middle" transform="rotate(0,141,297)" style="fill: #808080; fill-opacity: 0.75; font-family: \'Segoe UI\', \'Helvetica Neue\', \'Trebuchet MS\', Verdana; font-weight: 400; font-size: 12px; cursor: default;"><tspan x="141" dy="0">Tuesday</tspan></text><text x="221" y="297" text-anchor="middle" transform="rotate(0,221,297)" style="fill: #808080; fill-opacity: 0.75; font-family: \'Segoe UI\', \'Helvetica Neue\', \'Trebuchet MS\', Verdana; font-weight: 400; font-size: 12px; cursor: default;"><tspan x="221" dy="0">Wednesday</tspan></text><text x="301" y="297" text-anchor="middle" transform="rotate(0,301,297)" style="fill: #808080; fill-opacity: 0.75; font-family: \'Segoe UI\', \'Helvetica Neue\', \'Trebuchet MS\', Verdana; font-weight: 400; font-size: 12px; cursor: default;"><tspan x="301" dy="0">Thursday</tspan></text><text x="380" y="297" text-anchor="middle" transform="rotate(0,380,297)" style="fill: #808080; fill-opacity: 0.75; font-family: \'Segoe UI\', \'Helvetica Neue\', \'Trebuchet MS\', Verdana; font-weight: 400; font-size: 12px; cursor: default;"><tspan x="380" dy="0">Friday</tspan></text><text x="460" y="297" text-anchor="middle" transform="rotate(0,460,297)" style="fill: #808080; fill-opacity: 0.75; font-family: \'Segoe UI\', \'Helvetica Neue\', \'Trebuchet MS\', Verdana; font-weight: 400; font-size: 12px; cursor: default;"><tspan x="460" dy="0">Saturday</tspan></text><text x="539" y="297" text-anchor="middle" transform="rotate(0,539,297)" style="fill: #808080; fill-opacity: 0.75; font-family: \'Segoe UI\', \'Helvetica Neue\', \'Trebuchet MS\', Verdana; font-weight: 400; font-size: 12px; cursor: default;"><tspan x="539" dy="0">Sunday</tspan></text></g><g class="dxc-line"></g></g><g class="dxc-v-axis"><g class="dxc-grid"><path stroke-width="1" stroke="#808080" stroke-opacity="0.35" d="M 22 275.5 L 579 275.5"></path><path stroke-width="1" stroke="#808080" stroke-opacity="0.35" d="M 22 231.5 L 579 231.5"></path><path stroke-width="1" stroke="#808080" stroke-opacity="0.35" d="M 22 187.5 L 579 187.5"></path><path stroke-width="1" stroke="#808080" stroke-opacity="0.35" d="M 22 144.5 L 579 144.5"></path><path stroke-width="1" stroke="#808080" stroke-opacity="0.35" d="M 22 100.5 L 579 100.5"></path><path stroke-width="1" stroke="#808080" stroke-opacity="0.35" d="M 22 56.5 L 579 56.5"></path><path stroke-width="1" stroke="#808080" stroke-opacity="0.35" d="M 22 12.5 L 579 12.5"></path></g><g class="dxc-elements"><text x="12" y="279" text-anchor="end" style="fill: #808080; fill-opacity: 0.75; font-family: \'Segoe UI\', \'Helvetica Neue\', \'Trebuchet MS\', Verdana; font-weight: 400; font-size: 12px; cursor: default;"><tspan x="12" dy="0">0</tspan></text><text x="12" y="235" text-anchor="end" style="fill: #808080; fill-opacity: 0.75; font-family: \'Segoe UI\', \'Helvetica Neue\', \'Trebuchet MS\', Verdana; font-weight: 400; font-size: 12px; cursor: default;"><tspan x="12" dy="0">2</tspan></text><text x="12" y="191" text-anchor="end" style="fill: #808080; fill-opacity: 0.75; font-family: \'Segoe UI\', \'Helvetica Neue\', \'Trebuchet MS\', Verdana; font-weight: 400; font-size: 12px; cursor: default;"><tspan x="12" dy="0">4</tspan></text><text x="12" y="148" text-anchor="end" style="fill: #808080; fill-opacity: 0.75; font-family: \'Segoe UI\', \'Helvetica Neue\', \'Trebuchet MS\', Verdana; font-weight: 400; font-size: 12px; cursor: default;"><tspan x="12" dy="0">6</tspan></text><text x="12" y="104" text-anchor="end" style="fill: #808080; fill-opacity: 0.75; font-family: \'Segoe UI\', \'Helvetica Neue\', \'Trebuchet MS\', Verdana; font-weight: 400; font-size: 12px; cursor: default;"><tspan x="12" dy="0">8</tspan></text><text x="12" y="60" text-anchor="end" style="fill: #808080; fill-opacity: 0.75; font-family: \'Segoe UI\', \'Helvetica Neue\', \'Trebuchet MS\', Verdana; font-weight: 400; font-size: 12px; cursor: default;"><tspan x="12" dy="0">10</tspan></text><text x="12" y="16" text-anchor="end" style="fill: #808080; fill-opacity: 0.75; font-family: \'Segoe UI\', \'Helvetica Neue\', \'Trebuchet MS\', Verdana; font-weight: 400; font-size: 12px; cursor: default;"><tspan x="12" dy="0">12</tspan></text></g><g class="dxc-line"></g></g></g><g class="dxc-border"></g><g class="dxc-series-group"><g class="dxc-series" transform="translate(0,0) scale(1,1)"><g class="dxc-elements" clip-path="url(#DevExpress_3)"></g><g class="dxc-markers" stroke-width="0" stroke="none" fill="#ffa500" r="0" inh="true" stroke-dasharray="none" line-width="2"><rect x="34" y="209" width="56" height="66" rx="0" ry="0"></rect><rect x="113" y="231" width="56" height="44" rx="0" ry="0"></rect><rect x="193" y="209" width="56" height="66" rx="0" ry="0"></rect><rect x="273" y="187" width="56" height="88" rx="0" ry="0"></rect><rect x="352" y="144" width="56" height="131" rx="0" ry="0"></rect><rect x="432" y="34" width="56" height="241" rx="0" ry="0"></rect><rect x="511" y="187" width="56" height="88" rx="0" ry="0"></rect></g></g></g><g class="dxc-labels-group"><g class="dxc-series-labels" clip-path="url(#DevExpress_3)" opacity="1"></g></g><g class="dxc-tooltip"><path fill="#000000" stroke="none" opacity="0.1" d="M 0 0" visibility="hidden"></path><path d="M 0 0 Z" visibility="hidden"></path><text x="0" y="0" text-anchor="middle" visibility="hidden" style="font-family: \'Segoe UI Light\', \'Helvetica Neue Light\', \'Segoe UI\', \'Helvetica Neue\', \'Trebuchet MS\', Verdana; font-weight: 200; font-size: 26px; fill: #ffffff; fill-opacity: 0.75; cursor: default;"><tspan x="0" dy="0">0</tspan></text></g><g class="dxc-crosshair-cursor"></g><g class="dxc-trackers" opacity="0.0001"><g class="dxc-crosshair-trackers" stroke="none" fill="grey"></g><g class="dxc-series-trackers"><g class="dxc-pane-tracker"></g></g><g class="dxc-markers-trackers" stroke="none" fill="grey"><g class="dxc-pane-tracker" clip-path="url(#DevExpress_3)"><rect x="34" y="209" width="56" height="66" rx="0" ry="0"></rect><rect x="113" y="231" width="56" height="44" rx="0" ry="0"></rect><rect x="193" y="209" width="56" height="66" rx="0" ry="0"></rect><rect x="273" y="187" width="56" height="88" rx="0" ry="0"></rect><rect x="352" y="144" width="56" height="131" rx="0" ry="0"></rect><rect x="432" y="34" width="56" height="241" rx="0" ry="0"></rect><rect x="511" y="187" width="56" height="88" rx="0" ry="0"></rect></g></g><g class="dxc-legend-trackers" stroke="none" fill="grey" transform="translate(609,12)"><rect x="-10" y="-6" width="101" height="23" rx="0" ry="0"></rect></g></g></svg>';
 
-            setTimeout(function() {
+            //setTimeout(function() {
                 //$('#chart').css({'display':'none'});
-                ogreAPI.initializeCanvas();
-                ogreAPI.listeners.objectClick();
-                ogreAPI.text.modifyButton();
-                //var chart = document.getElementById('chart').innerHTML;
                 
-                var dataUrl = 'data:image/svg+xml,' + encodeURIComponent(test);
+                //var chart = document.getElementById('chart').innerHTML;
+
+                var chart = $('#charts-panel svg')[0].outerHTML;
+                
+                //var dataUrl = 'data:image/svg+xml,' + encodeURIComponent(test);
+
+                var dataUrl = 'data:image/svg+xml,' + encodeURIComponent(chart);
 
                 fabric.Image.fromURL(dataUrl, function(img) {
 
                     img.top = 100;
                     img.left = 100;
                     ogreAPI.canvas.add(img);
-                    ogreAPI.charts.push( img );
+                    ogreAPI.charts.chartElements.push( img );
                     img.myClass = "chart";
 
                 });
-            }, 100);
+           // }, 100);
         },
         colorPicker: function(){
 
@@ -361,11 +481,8 @@
                         }, 300);
                     }
                 });
-
-                $('#charts-panel .back-button').on('click', function() {
-                    $('#charts-wrapper').addClass('hide');
-                });
             },
+
             objectClick: function() {
                 //This can get the instance of an object for further modifications 
                 ogreAPI.canvas.on('object:selected', function(options) {
@@ -445,11 +562,13 @@
         
         $('.pager span').text(""); //Eliminate the text from bullets on the slider
 
-        ogreAPI.loadDummyInfographic();
+        ogreAPI.initializeCanvas();
 
-        //ogreAPI.listeners.objectClick();
+        ogreAPI.listeners.objectClick();
+
+        ogreAPI.text.modifyButton();
         
-        ogreAPI.listeners.loginButton();
+        //ogreAPI.listeners.loginButton();
 
         ogreAPI.listeners.menuItemButton();
 
@@ -474,5 +593,9 @@
         ogreAPI.listeners.removeButton();
 
         ogreAPI.exportInfographic.all();
+
+        ogreAPI.charts.addFileButon();
+
+        ogreAPI.charts.dropdownOptions();
     });
 })();
